d2cd1a5d7ea54a7fd5dba51b18dbe5d5029a5619
Anna <anna@priceva.com>
2025-04-09 08:16:41 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cocktail-app',
+  webDir: 'www'
+};
+
+export default config;
